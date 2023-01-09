ebd3c18df1fe7bc17d198996615d5bf9478e01ba
Lars <lars.kra2001@gmail.com>
2023-01-09 10:52:30 +0100

    fixed tims code

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 // Import our custom CSS
-// import '../src/scss/styles.scss'
-// import 'bootstrap-icons/font/bootstrap-icons.css'
+import '../src/scss/styles.scss'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
-// vite.config.js
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-const path = require('path')
+const path = require('path');
+import vuePugPlugin from 'vue-pug-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve:{
-    alias:{
-      '@' : path.resolve(__dirname, './src')
-    },
-  },
-  plugins: [vue()]
+  plugins: [vue(), vuePugPlugin],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap')
+    }
+  }
 })
