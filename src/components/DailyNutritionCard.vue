ed67450dd50388b4daf08782e569b5043bca4ecf
Lars <lars.kra2001@gmail.com>
2023-01-10 10:47:49 +0100

    added saving Meal functionality

--- src/components/DailyNutritionCard.vue
+++ src/components/DailyNutritionCard.vue
@@ -2,12 +2,35 @@
 import PolarArea from '../components/charts/PolarArea.vue';
 import DailyNutrition from '../components/charts/DailyNutrition.vue';
 import IngredientTable from '../components/IngredientTable.vue';
+import axios from 'axios';
 
 export default {
   components: {
     DailyNutrition,
     PolarArea,
     IngredientTable,
+  },
+  data () {
+    return {
+      mealName: ''
+    }
+  },
+  methods: {
+    async submitForm() {
+      try {
+        const token = 'miTQ1NwbocCI?A2uyop1?VN=l3wh?kebR6WuepYJCOFfzWqGImXfiO/Ksed5pAxQBP8km8qU!6RmhehCPlF5D7TZm?R8w4bH8JpQXxrgABVDfAHyC9yBp3M2zxCQN13-oSf-fJhqjY-X9HlyMyq6y3Rm486eOx5VGWt!upDx-Y3CorzLs747otpnGEcfOQozNoSzJqlC!PZGypR22j/2DD1jzuCml!eHjfkX=sT8lQYqabuOnAJ/fhI6HKdo1p0X'
+        const response = await axios.post('https://nutrition-calculation-app.onrender.com/api/v1/meals', {
+          mealName: this.mealName,
+        }, {
+          headers: {
+            'Authorization': 'Bearer ' + token
+          }
+        });
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
   }
 }
 </script>
@@ -37,9 +60,9 @@ export default {
           hr
           .card-text
             .align-middle
-              form(action="" method="")
+              form(@submit.prevent="submitForm")
                 .form-floating
-                  input#mealName.form-control(placeholder="Meal's name")
+                  input#mealName.form-control(name="mealName" placeholder="Meal's name")
                   label(for="mealName") Meal's name
                   .d-grid.gap-2.d-md-flex.justify-content-md-end.mt-2
                     button.btn.btn-success.me-md-2(type="submit") Save
